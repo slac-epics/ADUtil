b232889f1e615ea50b6b5f6a371636154d7c5fca
Luciano Piccoli <lpiccoli@slac.stanford.edu>
2015-02-12 22:28:41 +0000

    ADSupport-R1-1-0: Added support to tag images that are one PULSEID late when                   running at 120 Hz (event 140).

--- timeStampSourceLibApp/src/timeStampSource.c
+++ timeStampSourceLibApp/src/timeStampSource.c
@@ -1,6 +1,9 @@
 #include <epicsTime.h>
 #include <registryFunction.h>
 #include <epicsExport.h>
+#include <epicsMutex.h>
+#include <perfMeasure.h>
+#include <subRecord.h>            /* for struct subRecord         */
 #include "drvMrfEr.h"
 #include "evrPattern.h"
 #include "evrTime.h"
@@ -13,14 +16,8 @@
  *  Demonstrate the user-defined time source function
  */
 
-
-static void TimeStampSource(void *userPvt, epicsTimeStamp *pTimeStamp)
+static void TimeStampSourceProcess(adTimeStampSource_p pTss, epicsTimeStamp *pTimeStamp)
 {
-
-   adTimeStampSource_p  pTss = userPvt;
-
-   if(!pTss) return;
-
    epicsMutexLock (pTss->lock);
 
    ErGetTicks(0, &pTss->erTicks);
@@ -34,10 +31,114 @@ static void TimeStampSource(void *userPvt, epicsTimeStamp *pTimeStamp)
    pTss->diff_pulseID      = pTss->current_pulseID - pTss->active_pulseID;
 
    epicsMutexUnlock (pTss->lock);
+}
+
+static void TimeStampSource(void *userPvt, epicsTimeStamp *pTimeStamp)
+{
+   adTimeStampSource_p  pTss = userPvt;
+   if(!pTss) return;
+   TimeStampSourceProcess(pTss, pTimeStamp);
 
    scanIoRequest(pTss->ioScanPvt);
 }
 
 epicsRegisterFunction(TimeStampSource);
 
+/**
+ * The following variables are used for the pipelined TSS
+ */
+perfParm_ts *tssAcquisitionPerf;
+int tssAcquisitionPerfStarted = 0;
+epicsMutexId tssMutex;
 
+epicsTimeStamp tssTimeStamp0;
+epicsTimeStamp tssTimeStamp1;
+
+int tssTimeStampSource = 0;
+int tssSourceEvent = 0;
+
+/**
+ * Subroutine record initialization function invoked by the $(DEV):TSS_START
+ * record. This routine starts a perfMeasure measurement point, that gets
+ * started when $(DEV):TSS_START is processed and ends when the 
+ * PipelinedTimeStampSource() function is called by AD when the image is
+ * ready to be timestamped.
+ */
+static long tssStartInit(subRecord *prec) {
+  tssMutex = epicsMutexMustCreate();
+  tssAcquisitionPerf = makePerfMeasure("ImageAcquisition", "Time between trigger and image tagging");
+
+  return 0;
+}
+
+void getPulseId(int sourceEvent) {
+  epicsTimeStamp *timeStamp;
+
+  if (tssTimeStampSource == 0) {
+    timeStamp = &tssTimeStamp0;
+  }
+  else {
+    timeStamp = &tssTimeStamp1;
+  }
+
+  evrTimeGet(timeStamp, sourceEvent);
+  evrTimeGetFromPipeline(timeStamp, evrTimeCurrent, 0, 0,0,0,0);
+
+  if (sourceEvent == 140) {
+    tssTimeStampSource = !tssTimeStampSource;
+  }
+}
+
+static long tssStart(subRecord *prec) {
+  int sourceEvent = prec->a;
+  tssSourceEvent = sourceEvent;
+
+  epicsMutexLock(tssMutex);
+
+  getPulseId(sourceEvent);
+
+  if (tssAcquisitionPerfStarted == 0) {
+    startPerfMeasure(tssAcquisitionPerf);
+    tssAcquisitionPerfStarted = 1;
+  }
+  epicsMutexUnlock(tssMutex);
+
+  return 0;  
+}
+
+static void PipelinedTimeStampSource(void *userPvt, epicsTimeStamp *pTimeStamp)
+{
+   adTimeStampSource_p  pTss = userPvt;
+   if(!pTss) return;
+   TimeStampSourceProcess(pTss, pTimeStamp);
+
+   epicsMutexLock(tssMutex);
+
+   if (tssTimeStampSource == 0) {
+     pTimeStamp->secPastEpoch = tssTimeStamp0.secPastEpoch;
+     pTimeStamp->nsec = tssTimeStamp0.nsec;
+   }
+   else {
+     pTimeStamp->secPastEpoch = tssTimeStamp1.secPastEpoch;
+     pTimeStamp->nsec = tssTimeStamp1.nsec;
+   }
+
+   if (tssAcquisitionPerfStarted != 0)  {
+     if (tssAcquisitionPerfStarted == 1 && tssSourceEvent == 140) {
+       tssAcquisitionPerfStarted++;
+     }
+     else {
+       endPerfMeasure(tssAcquisitionPerf);
+       tssAcquisitionPerfStarted = 0;
+       calcPerfMeasure(tssAcquisitionPerf);
+     }
+   }
+
+   epicsMutexUnlock(tssMutex);
+
+   scanIoRequest(pTss->ioScanPvt);
+}
+
+epicsRegisterFunction(tssStartInit);
+epicsRegisterFunction(tssStart);
+epicsRegisterFunction(PipelinedTimeStampSource);
