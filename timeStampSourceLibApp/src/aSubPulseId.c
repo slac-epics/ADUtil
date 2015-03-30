80acd55f7faccb637c033f8fafd71d74e4eaf61e
Kukhee Kim <khkim@slac.stanford.edu>
2015-03-30 17:43:15 +0000

    add aSubPulseId database to implement the pulse id wavefrom

---
+++ timeStampSourceLibApp/src/aSubPulseId.c
@@ -0,0 +1,32 @@
+#include <aSubRecord.h>   i       /* for sgtruct aSubRecord */
+#include <registryFunction.h>     /* for epics register function  */
+#include <epicsExport.h>          /* for epicsExport              */
+
+
+#define LOWER_17BITS_MASK    (0x0001ffff)   /* mask for pulse id */
+
+static long aSubPulseIdInit(aSubRecord *prec)
+{
+    prec->udf = 0;
+
+    return 0;
+}
+
+static long aSubPulseId(aSubRecord *prec)
+{
+    int i, *a, *v;
+
+    a = (int *) prec->a;
+    v = (int *) prec->vala;
+
+    for(i = 0; i < prec->nea; i++) {
+        v[i] = a[i] & LOWER_17BITS_MASK;
+    }
+    prec->neva = prec->nea;
+
+    return 0;
+}
+
+
+epicsRegisterFunction(aSubPulseIdInit);
+epicsRegisterFunction(aSubPulseId);
